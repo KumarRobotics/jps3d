2479ffbb258c029c43d86043bd11a81e61218616
sikang <lskwdlskwd@gmail.com>
2017-05-18 17:02:47 -0400

    update docs

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['node',['Node',['../classJPS_1_1JPS3DUtil_1_1Node.html',1,'JPS::JPS3DUtil::Node'],['../classJPS_1_1JPS2DUtil_1_1Node.html',1,'JPS::JPS2DUtil::Node']]],
-  ['nodelinks',['NodeLinks',['../classNodeLinks.html',1,'']]],
-  ['nxjps3dutil',['NXJPS3DUtil',['../classNXJPS3DUtil.html',1,'']]]
+  ['nodelinks',['NodeLinks',['../classNodeLinks.html',1,'']]]
 ];
