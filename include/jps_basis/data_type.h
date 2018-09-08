be20eb33e40a2e8b6dd1d0a3e4807850826975c5
sikang <lskwdlskwd@gmail.com>
2018-09-08 03:36:26 -0400

    add Aff3f in data_type

--- include/jps_basis/data_type.h
+++ include/jps_basis/data_type.h
@@ -109,4 +109,9 @@ typedef MatDNf<3> MatD3f;
 ///Dynamic MxN Eigen float matrix
 typedef Matf<Eigen::Dynamic, Eigen::Dynamic> MatDf;
 
+///Allias of Eigen::Affine2d
+typedef Eigen::Transform<decimal_t, 2, Eigen::Affine> Aff2f;
+///Allias of Eigen::Affine3d
+typedef Eigen::Transform<decimal_t, 3, Eigen::Affine> Aff3f;
+
 #endif
